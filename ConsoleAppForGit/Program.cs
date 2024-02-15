c6b98ff8d1ee47eb938d5da12a070e9c026472ba
amank-sdm <aman.kumar@smartdatainc.net>
2024-02-15 16:50:03 +0530

    Add project files.

---
+++ ConsoleAppForGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleAppForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
